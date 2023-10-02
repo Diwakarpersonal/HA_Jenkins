d9d819f757790c839e431e37841fbda988c2332c
diwakar <diwakar.sharma@opstree.com>
2023-10-02 13:22:21 +0530

    a

--- modules/Network/main.tf
+++ modules/Network/main.tf
@@ -256,7 +256,10 @@ resource "aws_launch_template" "jenkins_template" {
 sudo apt-get update -y
 sudo apt-get install nfs-common -y
 sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.ninja.dns_name}:/ /var/lib/jenkins/
-sudo rsync -a /usr/local/jenkins_data/ /var/lib/jenkins/
+config_file="/var/lib/jenkins/config.xml"
+if [ ! -e "$config_file" ]; then
+  sudo rsync -a /usr/local/jenkins_data/ /var/lib/jenkins/
+fi
 sudo systemctl restart jenkins.service
 EOF
    )
